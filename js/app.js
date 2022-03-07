9e5792b89baaf069b26ebf714bcbce45718c8761
ahmed-shiiqaaye <me.shiqaye@gmail.com>
2022-03-08 01:31:31 +0200

    pricing tag

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 let menu = document.querySelector('.menu');
+let times = document.querySelector('.times');
 let links = document.querySelector('.links');
 menu.addEventListener('click',function(){
     links.classList.toggle('active')
+})
+times.addEventListener('click',function(){
+    links.classList.remove('active')
 })
